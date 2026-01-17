ed2006edb493c448a45c8055822c57970b1c4ee9
Kevinho71 <kevin.guzman@ucb.edu.bo>
2026-01-17 00:10:32 -0400

    serie de configuraciones para el arranque en produccion de la app listas

--- src/main/java/app/CadetappApplication.java
+++ src/main/java/app/CadetappApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class CadetappApplication extends SpringBootServletInitializer {
     
+   
     public static void main(String[] args) {
         SpringApplication.run(CadetappApplication.class, args);
     }
